d6c3b7a87ca5acd6aa44ac80425da7f2e3d9b14c
neel0390 <katkarneel@gmail.com>
2023-01-25 18:44:08 +0530

    Done all!

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Home = () => {
+  return (
+    <div style={{ margin: "50px 50px", fontSize: "30px", textAlign: "center" }}>
+      <h2 style={{ color: "orange" }}>Welcome to Home Page!</h2>
+    </div>
+  );
+};
+export default Home;
